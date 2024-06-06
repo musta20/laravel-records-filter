b6e39f74148f525de3f0cd7e2c5170372794b767
musta20 <must.g20@gmail.com>
2024-06-06 05:48:22 +0300

    refactor rel component

--- src/resources/views/components/relations-option.blade.php
+++ src/resources/views/components/relations-option.blade.php
@@ -13,33 +13,77 @@
 
 
 
-
-    <!-- Dropdown menu -->
     <div x-cloak x-show="openMenu" id="dropdownAction"
         class="z-10 mt-10 absolute bg-white divide-y divide-gray-100 rounded-lg shadow w-50  ">
-        <ul class="py-1 text-sm text-gray-700 " aria-labelledby="dropdownActionButton">
-            @foreach ($relationsFilterOptions as $item)
-            <li>
-                <div class="block px-4 py-2 hover:bg-gray-100" >
-                    <label>{{$item['label']}}</label>
-                    <select 
-                    class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg  bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
 
-                    >
-                        @foreach ($item['data'] as $option)
-                        <option value="{{$option->id}}">{{$option->name}}</option>
-                        @endforeach
-                    </select>
+        <form @submit.prevent="submitFilter" id="rel" method="get">
+            <ul class="py-1 text-sm text-gray-700 " aria-labelledby="dropdownActionButton">
+
+                @php
+
+                $arrayIndexCounter = 0;
+
+                if (request()->has('rel')){
+                    $relRequestData = collect(request()->get('rel'));
+                }
+
+                @endphp
+
+                @foreach ($relationsFilterOptions as $item)
+
+                <li>
+                    <div class="block px-4 py-2 hover:bg-gray-100">
+                        <label>{{$item['label']}}</label>
+                        <input value="{{$item['id']}}" name="rel[{{$arrayIndexCounter}}][filed]" hidden>
+
+                        <select 
+
+                            name="rel[{{$arrayIndexCounter}}][value]"
+
+                            class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg  bg-gray-50 focus:ring-blue-500 focus:border-blue-500 ">
+
+                            <option disabled 
+                                @if (!isset($relRequestData) || !$relRequestData->contains('filed', $item['id']))
+                                selected 
+                                @endif value> 
+                                    -- select an option --
+                            </option>
 
 
-                </div>
-                {{-- <a href="{{$paginator->buildUri(['rel'=>$relType,'id'=>$item->id])}}"
-                    class="block px-4 py-2 hover:bg-gray-100  ">
-                    {{$item->name ?? $item->title}}
-                </a> --}}
-            </li>
-            @endforeach
-        </ul>
+                            @foreach ($item['data'] as $option)
+                            <option
+                            
+                            @if (isset($relRequestData) && $relRequestData->where('filed', $item['id'])->where('value', $option->id)->first())
 
+                                selected
+
+                            @endif
+                                
+                                value="{{$option->id}}">
+
+                                {{$option->name}}
+
+                            </option>
+                            @php
+                            $arrayIndexCounter++;
+                            @endphp
+                            @endforeach
+                        </select>
+
+
+                    </div>
+
+                </li>
+                @php
+                $arrayIndexCounter++;
+                @endphp
+                @endforeach
+            </ul>
+            <button
+                class="w-full text-white m-5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                type="submit">
+                {{__('filter')}}
+            </button>
+        </form>
     </div>
 </div>
